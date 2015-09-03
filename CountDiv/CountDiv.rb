43b3f9e5a09865e6321a7be6f906801a3ecaa4ee
Bob Cao <bobintornado@gmail.com>
2015-09-03 11:03:03 +0800

    update

--- CountDiv/CountDiv.rb
+++ CountDiv/CountDiv.rb
@@ -1,11 +1,5 @@
 def solution(a, b, k)
-  # write your code in Ruby 2.2
   n1 = (a - a % k)/k
   n2 = (b - b % k)/k
-  r = n2 - n1
-  if  (a % k) == 0
-    (r + 1)
-  else
-    r
-  end
+  (a % k) == 0 ? (n2 - n1 + 1) : n2 - n1
 end
