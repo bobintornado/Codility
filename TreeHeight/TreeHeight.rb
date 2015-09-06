a17e03adb5c8369b0a4b0e97a82e94e315678549
Bob Cao <bobintornado@gmail.com>
2015-09-06 16:45:44 +0800

    update

--- TreeHeight/TreeHeight.rb
+++ TreeHeight/TreeHeight.rb
@@ -1,20 +1,20 @@
 def solution(t)
-    return -1 if t.x.nil?
+  return -1 if t.x.nil?
 
-    $max_depth = current_depth = 0 
-    recur_traverse(t, current_depth)
+  $max_depth = current_depth = 0
+  recur_traverse(t, current_depth)
 
-    return $max_depth
+  return $max_depth
 end
 
 def recur_traverse(t, current_depth)
-	if !t.l.nil?
-		$max_depth = [current_depth+1,$max_depth].max
-		recur_traverse(t.l,current_depth+1)
-	end
+  if !t.l.nil?
+    $max_depth = [current_depth+1,$max_depth].max
+    recur_traverse(t.l,current_depth+1)
+  end
 
-	if !t.r.nil?
-		$max_depth = [current_depth+1,$max_depth].max
-		recur_traverse(t.r,current_depth+1)
-	end
+  if !t.r.nil?
+    $max_depth = [current_depth+1,$max_depth].max
+    recur_traverse(t.r,current_depth+1)
+  end
 end
